c3236bc8eeea929fcbf2e66daac8e4d6cf76bed1
Jordan Widdison <jordan.widdison@gmail.com>
2019-11-11 17:36:12 -0700

    Updates

---
+++ precache-manifest.088efa3c00be778c27fc2729e2ae83d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c26746282705595f25f5f46bb7f11e",
+    "url": "/jwiddison/2048js/index.html"
+  },
+  {
+    "revision": "c041704c93fe0786ab47",
+    "url": "/jwiddison/2048js/static/css/main.0a67f3bb.chunk.css"
+  },
+  {
+    "revision": "7f780ca39ae3ccfe9db4",
+    "url": "/jwiddison/2048js/static/js/2.b712317e.chunk.js"
+  },
+  {
+    "revision": "c041704c93fe0786ab47",
+    "url": "/jwiddison/2048js/static/js/main.19487749.chunk.js"
+  },
+  {
+    "revision": "7de059b85a75f9cb5875",
+    "url": "/jwiddison/2048js/static/js/runtime-main.05c42667.js"
+  }
+]);
